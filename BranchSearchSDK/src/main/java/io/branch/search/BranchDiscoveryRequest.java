e0940ebd1a55e81bbb3d918bf0f779ea32ed1461
Mattia Iavarone <mat.iavarone@gmail.com>
2019-12-13 14:23:47 +0100

    Add BranchDiscoveryRequest.addExtra()

--- BranchSearchSDK/src/main/java/io/branch/search/BranchDiscoveryRequest.java
+++ BranchSearchSDK/src/main/java/io/branch/search/BranchDiscoveryRequest.java
@@ -1,24 +1,31 @@
 package io.branch.search;
 
 import android.location.Location;
+import android.support.annotation.NonNull;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Request model for Branch Discovery.
  */
+@SuppressWarnings("unchecked")
 public class BranchDiscoveryRequest<T extends BranchDiscoveryRequest> {
 
     enum JSONKey {
         Latitude("user_latitude"),
         Longitude("user_longitude"),
-        Timestamp("utc_timestamp");
+        Timestamp("utc_timestamp"),
+        Extra("extra_data");
 
         JSONKey(String key) {
             _key = key;
         }
 
+        @NonNull
         @Override
         public String toString() {
             return _key;
@@ -32,6 +39,8 @@ public class BranchDiscoveryRequest<T extends BranchDiscoveryRequest> {
 
     // Longitude for the user
     private double user_longitude;
+
+    private Map<String, Object> extra_data = new HashMap<>();
 
     /**
      * Private Constructor.
@@ -50,7 +59,7 @@ public class BranchDiscoveryRequest<T extends BranchDiscoveryRequest> {
             setLatitude(location.getLatitude());
             setLongitude(location.getLongitude());
         }
-        return (T)this;
+        return (T) this;
     }
 
     /**
@@ -58,9 +67,10 @@ public class BranchDiscoveryRequest<T extends BranchDiscoveryRequest> {
      * @param latitude latitude
      * @return this BranchDiscoveryRequest
      */
+    @SuppressWarnings({"UnusedReturnValue", "WeakerAccess"})
     public T setLatitude(double latitude) {
         this.user_latitude = latitude;
-        return (T)this;
+        return (T) this;
     }
 
     /**
@@ -68,9 +78,22 @@ public class BranchDiscoveryRequest<T extends BranchDiscoveryRequest> {
      * @param longitude latitude
      * @return this BranchDiscoveryRequest
      */
+    @SuppressWarnings({"UnusedReturnValue", "WeakerAccess"})
     public T setLongitude(double longitude) {
         this.user_longitude = longitude;
-        return (T)this;
+        return (T) this;
+    }
+
+    /**
+     * Adds extra data to be passed to server in forms
+     * of a key-value pair.
+     * @param key a key
+     * @param data value
+     * @return this BranchDiscoveryRequest
+     */
+    public T addExtra(@NonNull String key, @NonNull Object data) {
+        this.extra_data.put(key, data);
+        return (T) this;
     }
 
     JSONObject convertToJson(JSONObject jsonObject) {
@@ -81,6 +104,16 @@ public class BranchDiscoveryRequest<T extends BranchDiscoveryRequest> {
             // Add the current timestamp.
             Long tsLong = System.currentTimeMillis();
             jsonObject.putOpt(JSONKey.Timestamp.toString(), tsLong);
+
+            // Add extra data.
+            if (!extra_data.keySet().isEmpty()) {
+                JSONObject extraData = new JSONObject();
+                for (String key : extra_data.keySet()) {
+                    Object value = extra_data.get(key);
+                    extraData.putOpt(key, value);
+                }
+                jsonObject.putOpt(JSONKey.Extra.toString(), extraData);
+            }
 
 
         } catch (JSONException ignore) {
