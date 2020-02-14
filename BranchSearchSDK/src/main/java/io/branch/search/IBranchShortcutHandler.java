ad58d1c74c597222a7da6cdf97af4cbca16513e8
Mattia Iavarone <mat.iavarone@gmail.com>
2020-02-14 19:08:59 -0300

    Remove shortcut cache

--- BranchSearchSDK/src/main/java/io/branch/search/IBranchShortcutHandler.java
+++ BranchSearchSDK/src/main/java/io/branch/search/IBranchShortcutHandler.java
@@ -42,51 +42,28 @@ public interface IBranchShortcutHandler {
      * The default shortcut handler.
      */
     IBranchShortcutHandler DEFAULT = new IBranchShortcutHandler() {
-        // Caching values between results of the same query and across queries.
-        // This is important because querying is not so fast.
-        // https://developer.android.com/reference/android/content/pm/LauncherApps.ShortcutQuery
-        private final Map<String, Set<String>> mCache = new HashMap<>();
-
         @Override
         public boolean validateShortcut(@NonNull Context context,
                                         @NonNull String id,
                                         @NonNull String packageName) {
             if (Build.VERSION.SDK_INT < 25) return false;
-            if (!mCache.containsKey(packageName)) {
-                LauncherApps launcherApps = context.getSystemService(LauncherApps.class);
-                try {
-                    Set<String> ids = new HashSet<>();
-                    LauncherApps.ShortcutQuery query = new LauncherApps.ShortcutQuery();
-                    query.setQueryFlags(LauncherApps.ShortcutQuery.FLAG_MATCH_DYNAMIC
-                            | LauncherApps.ShortcutQuery.FLAG_MATCH_MANIFEST
-                            | LauncherApps.ShortcutQuery.FLAG_MATCH_PINNED);
-                    query.setPackage(packageName);
-                    List<ShortcutInfo> shortcuts = launcherApps.getShortcuts(query, Process.myUserHandle());
-                    if (shortcuts != null) {
-                        for (ShortcutInfo shortcut : shortcuts) {
-                            if (shortcut.isEnabled()) ids.add(shortcut.getId());
+            LauncherApps launcherApps = context.getSystemService(LauncherApps.class);
+            try {
+                LauncherApps.ShortcutQuery query = new LauncherApps.ShortcutQuery();
+                query.setQueryFlags(LauncherApps.ShortcutQuery.FLAG_MATCH_DYNAMIC
+                        | LauncherApps.ShortcutQuery.FLAG_MATCH_MANIFEST
+                        | LauncherApps.ShortcutQuery.FLAG_MATCH_PINNED);
+                query.setPackage(packageName);
+                List<ShortcutInfo> shortcuts = launcherApps.getShortcuts(query, Process.myUserHandle());
+                if (shortcuts != null) {
+                    for (ShortcutInfo shortcut : shortcuts) {
+                        if (shortcut.isEnabled() && shortcut.getId().equals(id)) {
+                            return true;
                         }
                     }
-                    mCache.put(packageName, ids);
-                } catch (Exception e) {
-                    // Not a launcher, not installed, invalid, ....
                 }
-            }
-            if (mCache.containsKey(packageName)) {
-                //noinspection ConstantConditions
-                if (mCache.get(packageName).contains(id)) {
-                    // Could return true, but first confirm that package is (still) installed.
-                    // LauncherApps.getApplicationInfo() would work but is API 26+, so we're
-                    // using package manager to cover API 25.
-                    PackageManager manager = context.getPackageManager();
-                    try {
-                        PackageInfo info = manager.getPackageInfo(packageName, 0);
-                        if (info != null) return true;
-                    } catch (PackageManager.NameNotFoundException ignore) { }
-                    // Package has been uninstalled after we saved its cache.
-                    // Remove the entry so we re-query next time.
-                    mCache.remove(packageName);
-                }
+            } catch (Exception e) {
+                // Not a launcher, not installed, invalid, ....
             }
             return false;
         }
